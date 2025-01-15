f36e1953ef149883d7eb78a440561248ddfd8f22
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2025-01-16 02:48:20 +0900

    tmp

--- discord-bot.py
+++ discord-bot.py
@@ -1,4 +1,6 @@
 import datetime
+from pydoc import describe
+
 import discord
 from discord import app_commands
 from discord.app_commands import Choice
@@ -43,7 +45,20 @@ async def set_channel(ctx):
     # ここにチャンネルを設定する処理を追加
     await ctx.response.send_message("チャンネルを設定するコマンドはまだ実装されていません。")
 
-@tree.command(name='week-time', description="毎週通知します")
+@tree.command(name='month-time',description='毎月通知します')
+@app_commands.describe(time="時間設定 hh:mm 例(6:00:00)")
+async def month_time(ctx:discord.Interaction,day:str,time:str,role:discord.Role=None):
+    try:
+        day = int(day)
+        data_time = datetime.datetime.strptime(time, '%H:%M:%S').time()
+        if role is not None:
+            db.set(str(ctx.guild.id), str(ctx.channel.id), "month", f"{day}-{data_time}", str(role.id))
+        else:
+            db.set(str(ctx.guild.id), str(ctx.channel.id), "month", f"{day}-{data_time}", "None")
+        await ctx.response.send_message(embed=say_embed(color=0x00ff00,title="設定完了",message=f"毎月{day}-{data_time} にメッセージを送信するように設定しました"))
+    except:
+       await ctx.response.send_message(embed=say_embed(color=0xff0000,title="失敗",message="日付、時間の形式が間違えています。"))
+@tree.command(name='week-time', description='毎週通知します')
 @app_commands.describe(week="曜日")
 @app_commands.choices(week=[
     Choice(name="月", value="monday"),
@@ -58,8 +73,11 @@ async def set_channel(ctx):
 async def week_time(ctx: discord.Interaction, week: Choice[str],time:str,role:discord.Role=None): # 引数名をdaysからweekに変更
     try:
         data_time = datetime.datetime.strptime(time, '%H:%M:%S').time()
-        db.set(str(ctx.guild.id),str(ctx.channel.id),"week",f"{week.name}-{data_time}",str(role.id))
-        await ctx.response.send_message(embed=say_embed(color=0x00ff00,title="設定完了",message=f"毎週{week.name}曜日 {data_time} にメッセージを送信するように設定しました"))
+        if role is not None:
+            db.set(str(ctx.guild.id),str(ctx.channel.id),"week",f"{week.name}-{data_time}",str(role.id))
+        else:
+            db.set(str(ctx.guild.id),str(ctx.channel.id),"week",f"{week.name}-{data_time}","None")
+        await ctx.response.send_message(embed=say_embed(color=0x00ff00,title="設定完了",message=f"毎週 {week.name} 曜日 {data_time} にメッセージを送信するように設定しました"))
     except:
         await ctx.response.send_message(embed=say_embed(color=0xff0000, title="エラー", message="設定中にエラーが発生しました"))
 @tree.command(name='day-time', description="毎日通知します")
@@ -67,7 +85,10 @@ async def week_time(ctx: discord.Interaction, week: Choice[str],time:str,role:di
 async def week_time(ctx: discord.Interaction,time:str,role:discord.Role=None): # 引数名をdaysからweekに変更
     try:
         data_time = datetime.datetime.strptime(time, '%H:%M:%S').time()
-        db.set(str(ctx.guild.id),str(ctx.channel.id),"day",str(data_time),str(role.id))
+        if role is not None:
+            db.set(str(ctx.guild.id),str(ctx.channel.id),"day",str(data_time),str(role.id))
+        else:
+            db.set(str(ctx.guild.id),str(ctx.channel.id),"day",str(data_time),"None")
         await ctx.response.send_message(embed=say_embed(color=0x00ff00,title="設定完了",message=f"毎日 {data_time} にメッセージを送信するように設定しました"))
     except:
         await ctx.response.send_message(embed=say_embed(color=0xff0000, title="エラー", message="設定中にエラーが発生しました"))
